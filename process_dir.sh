4e113c867dc1fcb5195615173a5f4d531fe37ebc
AnnabellaM <annabellamiao@gmail.com>
2024-06-22 21:50:38 -0500

    refactor dir structure

--- process_dir.sh
+++ process_dir.sh
@@ -1,42 +1,44 @@
-# cd dir/dir
-# make clean
-# mv ../dir ../../dir_cp
-# cd ..
-# rm -r dir
-# mv dir_cp dir
-
 #!/bin/bash
 
-directories=(
-    "equal"
-    "equal2"
-    "equal3"
-    "loop"
-    "magic"
-    "magic2"
-    "magic3"
-    "magicOne"
-    "recursive"
-)
+# directories=(
+#     "equal"
+#     "equal2"
+#     "equal3"
+#     "loop"
+#     "magic"
+#     "magic2"
+#     "magic3"
+#     "magicOne"
+#     "recursive"
+# )
 
-for dir in "${directories[@]}"; do
+# for dir in "${directories[@]}"; do
 
-    cd "$dir/$dir"
-    echo "Navigating to $dir"
+#     cd "$dir/$dir"
+#     echo "Navigating to $dir"
 
-    make clean
-    echo "Ran 'make clean' in $dir"
+#     make clean
+#     echo "Ran 'make clean' in $dir"
 
-    mv "../$dir" "../../${dir}_cp"
+#     mv "../$dir" "../../${dir}_cp"
 
-    cd ..
+#     cd ..
 
-    rm -r "$dir"
-    echo "Removed $dir"
+#     rm -r "$dir"
+#     echo "Removed $dir"
 
-    mv "${dir}_cp" "$dir"
-    echo "Renamed ${dir}_cp to $dir"
+#     mv "${dir}_cp" "$dir"
+#     echo "Renamed ${dir}_cp to $dir"
+# done
+
+# echo "Actions completed in all directories."
+
+find . -type f -name 'Makefile' | while read -r makefile; do
+    echo "Processing $makefile"
+    
+    sed -i '/^CC = \/AFLplusplus\/afl-clang-fast$/d' "$makefile"
+    
+    echo "Updated $makefile"
 done
 
-echo "Actions completed in all directories."
-
+echo "All Makefiles have been processed."
