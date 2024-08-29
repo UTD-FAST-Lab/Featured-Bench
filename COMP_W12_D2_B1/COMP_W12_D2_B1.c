87dc9d4553913db36927e12b6164b095aed36398
AnnabellaM <annabellamiao@gmail.com>
2024-08-28 23:40:33 -0500

    comp

---
+++ COMP_W12_D2_B1/COMP_W12_D2_B1.c
@@ -0,0 +1,367 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "getHash.h"
+
+void COMP_W12_D2_B1(unsigned hash)
+{
+    if (hash < 12) {
+        if (hash < 1) {
+            int *ptr = NULL;
+            *ptr = 10;
+            printf("this is branch 1\n");
+        } else if (hash < 2) {
+            printf("this is branch 2\n");
+        } else if (hash < 3) {
+            printf("this is branch 3\n");
+        } else if (hash < 4) {
+            printf("this is branch 4\n");
+        } else if (hash < 5) {
+            printf("this is branch 5\n");
+        } else if (hash < 6) {
+            printf("this is branch 6\n");
+        } else if (hash < 7) {
+            printf("this is branch 7\n");
+        } else if (hash < 8) {
+            printf("this is branch 8\n");
+        } else if (hash < 9) {
+            printf("this is branch 9\n");
+        } else if (hash < 10) {
+            printf("this is branch 10\n");
+        } else if (hash < 11) {
+            printf("this is branch 11\n");
+        } else if (hash < 12) {
+            printf("this is branch 12\n");
+        }
+    } else if (hash < 24) {
+        if (hash < 13) {
+            printf("this is branch 13\n");
+        } else if (hash < 14) {
+            printf("this is branch 14\n");
+        } else if (hash < 15) {
+            printf("this is branch 15\n");
+        } else if (hash < 16) {
+            printf("this is branch 16\n");
+        } else if (hash < 17) {
+            printf("this is branch 17\n");
+        } else if (hash < 18) {
+            printf("this is branch 18\n");
+        } else if (hash < 19) {
+            printf("this is branch 19\n");
+        } else if (hash < 20) {
+            printf("this is branch 20\n");
+        } else if (hash < 21) {
+            printf("this is branch 21\n");
+        } else if (hash < 22) {
+            printf("this is branch 22\n");
+        } else if (hash < 23) {
+            printf("this is branch 23\n");
+        } else if (hash < 24) {
+            printf("this is branch 24\n");
+        }
+    } else if (hash < 36) {
+        if (hash < 25) {
+            printf("this is branch 25\n");
+        } else if (hash < 26) {
+            printf("this is branch 26\n");
+        } else if (hash < 27) {
+            printf("this is branch 27\n");
+        } else if (hash < 28) {
+            printf("this is branch 28\n");
+        } else if (hash < 29) {
+            printf("this is branch 29\n");
+        } else if (hash < 30) {
+            printf("this is branch 30\n");
+        } else if (hash < 31) {
+            printf("this is branch 31\n");
+        } else if (hash < 32) {
+            printf("this is branch 32\n");
+        } else if (hash < 33) {
+            printf("this is branch 33\n");
+        } else if (hash < 34) {
+            printf("this is branch 34\n");
+        } else if (hash < 35) {
+            printf("this is branch 35\n");
+        } else if (hash < 36) {
+            printf("this is branch 36\n");
+        }
+    } else if (hash < 48) {
+        if (hash < 37) {
+            printf("this is branch 37\n");
+        } else if (hash < 38) {
+            printf("this is branch 38\n");
+        } else if (hash < 39) {
+            printf("this is branch 39\n");
+        } else if (hash < 40) {
+            printf("this is branch 40\n");
+        } else if (hash < 41) {
+            printf("this is branch 41\n");
+        } else if (hash < 42) {
+            printf("this is branch 42\n");
+        } else if (hash < 43) {
+            printf("this is branch 43\n");
+        } else if (hash < 44) {
+            printf("this is branch 44\n");
+        } else if (hash < 45) {
+            printf("this is branch 45\n");
+        } else if (hash < 46) {
+            printf("this is branch 46\n");
+        } else if (hash < 47) {
+            printf("this is branch 47\n");
+        } else if (hash < 48) {
+            printf("this is branch 48\n");
+        }
+    } else if (hash < 60) {
+        if (hash < 49) {
+            printf("this is branch 49\n");
+        } else if (hash < 50) {
+            printf("this is branch 50\n");
+        } else if (hash < 51) {
+            printf("this is branch 51\n");
+        } else if (hash < 52) {
+            printf("this is branch 52\n");
+        } else if (hash < 53) {
+            printf("this is branch 53\n");
+        } else if (hash < 54) {
+            printf("this is branch 54\n");
+        } else if (hash < 55) {
+            printf("this is branch 55\n");
+        } else if (hash < 56) {
+            printf("this is branch 56\n");
+        } else if (hash < 57) {
+            printf("this is branch 57\n");
+        } else if (hash < 58) {
+            printf("this is branch 58\n");
+        } else if (hash < 59) {
+            printf("this is branch 59\n");
+        } else if (hash < 60) {
+            printf("this is branch 60\n");
+        }
+    } else if (hash < 72) {
+        if (hash < 61) {
+            printf("this is branch 61\n");
+        } else if (hash < 62) {
+            printf("this is branch 62\n");
+        } else if (hash < 63) {
+            printf("this is branch 63\n");
+        } else if (hash < 64) {
+            printf("this is branch 64\n");
+        } else if (hash < 65) {
+            printf("this is branch 65\n");
+        } else if (hash < 66) {
+            printf("this is branch 66\n");
+        } else if (hash < 67) {
+            printf("this is branch 67\n");
+        } else if (hash < 68) {
+            printf("this is branch 68\n");
+        } else if (hash < 69) {
+            printf("this is branch 69\n");
+        } else if (hash < 70) {
+            printf("this is branch 70\n");
+        } else if (hash < 71) {
+            printf("this is branch 71\n");
+        } else if (hash < 72) {
+            printf("this is branch 72\n");
+        }
+    } else if (hash < 84) {
+        if (hash < 73) {
+            printf("this is branch 73\n");
+        } else if (hash < 74) {
+            printf("this is branch 74\n");
+        } else if (hash < 75) {
+            printf("this is branch 75\n");
+        } else if (hash < 76) {
+            printf("this is branch 76\n");
+        } else if (hash < 77) {
+            printf("this is branch 77\n");
+        } else if (hash < 78) {
+            printf("this is branch 78\n");
+        } else if (hash < 79) {
+            printf("this is branch 79\n");
+        } else if (hash < 80) {
+            printf("this is branch 80\n");
+        } else if (hash < 81) {
+            printf("this is branch 81\n");
+        } else if (hash < 82) {
+            printf("this is branch 82\n");
+        } else if (hash < 83) {
+            printf("this is branch 83\n");
+        } else if (hash < 84) {
+            printf("this is branch 84\n");
+        }
+    } else if (hash < 96) {
+        if (hash < 85) {
+            printf("this is branch 85\n");
+        } else if (hash < 86) {
+            printf("this is branch 86\n");
+        } else if (hash < 87) {
+            printf("this is branch 87\n");
+        } else if (hash < 88) {
+            printf("this is branch 88\n");
+        } else if (hash < 89) {
+            printf("this is branch 89\n");
+        } else if (hash < 90) {
+            printf("this is branch 90\n");
+        } else if (hash < 91) {
+            printf("this is branch 91\n");
+        } else if (hash < 92) {
+            printf("this is branch 92\n");
+        } else if (hash < 93) {
+            printf("this is branch 93\n");
+        } else if (hash < 94) {
+            printf("this is branch 94\n");
+        } else if (hash < 95) {
+            printf("this is branch 95\n");
+        } else if (hash < 96) {
+            printf("this is branch 96\n");
+        }
+    } else if (hash < 108) {
+        if (hash < 97) {
+            printf("this is branch 97\n");
+        } else if (hash < 98) {
+            printf("this is branch 98\n");
+        } else if (hash < 99) {
+            printf("this is branch 99\n");
+        } else if (hash < 100) {
+            printf("this is branch 100\n");
+        } else if (hash < 101) {
+            printf("this is branch 101\n");
+        } else if (hash < 102) {
+            printf("this is branch 102\n");
+        } else if (hash < 103) {
+            printf("this is branch 103\n");
+        } else if (hash < 104) {
+            printf("this is branch 104\n");
+        } else if (hash < 105) {
+            printf("this is branch 105\n");
+        } else if (hash < 106) {
+            printf("this is branch 106\n");
+        } else if (hash < 107) {
+            printf("this is branch 107\n");
+        } else if (hash < 108) {
+            printf("this is branch 108\n");
+        }
+    } else if (hash < 120) {
+        if (hash < 109) {
+            printf("this is branch 109\n");
+        } else if (hash < 110) {
+            printf("this is branch 110\n");
+        } else if (hash < 111) {
+            printf("this is branch 111\n");
+        } else if (hash < 112) {
+            printf("this is branch 112\n");
+        } else if (hash < 113) {
+            printf("this is branch 113\n");
+        } else if (hash < 114) {
+            printf("this is branch 114\n");
+        } else if (hash < 115) {
+            printf("this is branch 115\n");
+        } else if (hash < 116) {
+            printf("this is branch 116\n");
+        } else if (hash < 117) {
+            printf("this is branch 117\n");
+        } else if (hash < 118) {
+            printf("this is branch 118\n");
+        } else if (hash < 119) {
+            printf("this is branch 119\n");
+        } else if (hash < 120) {
+            printf("this is branch 120\n");
+        }
+    } else if (hash < 132) {
+        if (hash < 121) {
+            printf("this is branch 121\n");
+        } else if (hash < 122) {
+            printf("this is branch 122\n");
+        } else if (hash < 123) {
+            printf("this is branch 123\n");
+        } else if (hash < 124) {
+            printf("this is branch 124\n");
+        } else if (hash < 125) {
+            printf("this is branch 125\n");
+        } else if (hash < 126) {
+            printf("this is branch 126\n");
+        } else if (hash < 127) {
+            printf("this is branch 127\n");
+        } else if (hash < 128) {
+            printf("this is branch 128\n");
+        } else if (hash < 129) {
+            printf("this is branch 129\n");
+        } else if (hash < 130) {
+            printf("this is branch 130\n");
+        } else if (hash < 131) {
+            printf("this is branch 131\n");
+        } else if (hash < 132) {
+            printf("this is branch 132\n");
+        }
+    } else if (hash < 144) {
+        if (hash < 133) {
+            printf("this is branch 133\n");
+        } else if (hash < 134) {
+            printf("this is branch 134\n");
+        } else if (hash < 135) {
+            printf("this is branch 135\n");
+        } else if (hash < 136) {
+            printf("this is branch 136\n");
+        } else if (hash < 137) {
+            printf("this is branch 137\n");
+        } else if (hash < 138) {
+            printf("this is branch 138\n");
+        } else if (hash < 139) {
+            printf("this is branch 139\n");
+        } else if (hash < 140) {
+            printf("this is branch 140\n");
+        } else if (hash < 141) {
+            printf("this is branch 141\n");
+        } else if (hash < 142) {
+            printf("this is branch 142\n");
+        } else if (hash < 143) {
+            printf("this is branch 143\n");
+        } else if (hash < 144) {
+            printf("this is branch 144\n");
+        }
+    }
+
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "rb");
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        return 1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+    printf("size: %ld\n", size);
+
+    unsigned char *data = malloc(size * sizeof(unsigned char));
+    if (data == NULL)
+    {
+        fprintf(stderr, "Error allocating memory\n");
+        fclose(fp);
+        return 1;
+    }
+
+    if (fread(data, sizeof(unsigned char), size, fp) != size)
+    {
+        fprintf(stderr, "Error reading file\n");
+        fclose(fp);
+        free(data);
+        return 1;
+    }
+
+    COMP_W12_D2_B1(getHash(data, size));
+
+    free(data);
+
+    return 0;
+}
