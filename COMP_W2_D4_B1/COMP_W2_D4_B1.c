132ad541e45e2d7a6597ca51f16c49a56d38d6f9
AnnabellaM <annabellamiao@gmail.com>
2024-10-04 20:10:07 -0500

    comp_lib

---
+++ COMP_W2_D4_B1/COMP_W2_D4_B1.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+void COMP_W2_D4_B1(unsigned hash)
+{
+    if (hash < 8) {
+        if (hash < 4) {
+            if (hash < 2) {
+                if (hash < 1) {
+                    printf("this is branch 1\n");
+                    int *ptr = NULL;
+                    *ptr = 10;
+                } else if (hash < 2) {
+                    printf("this is branch 2\n");
+                }
+            } else if (hash < 4) {
+                if (hash < 3) {
+                    printf("this is branch 3\n");
+                } else if (hash < 4) {
+                    printf("this is branch 4\n");
+                }
+            }
+        } else if (hash < 8) {
+            if (hash < 6) {
+                if (hash < 5) {
+                    printf("this is branch 5\n");
+                } else if (hash < 6) {
+                    printf("this is branch 6\n");
+                }
+            } else if (hash < 8) {
+                if (hash < 7) {
+                    printf("this is branch 7\n");
+                } else if (hash < 8) {
+                    printf("this is branch 8\n");
+                }
+            }
+        }
+    } else if (hash < 16) {
+        if (hash < 12) {
+            if (hash < 10) {
+                if (hash < 9) {
+                    printf("this is branch 9\n");
+                } else if (hash < 10) {
+                    printf("this is branch 10\n");
+                }
+            } else if (hash < 12) {
+                if (hash < 11) {
+                    printf("this is branch 11\n");
+                } else if (hash < 12) {
+                    printf("this is branch 12\n");
+                }
+            }
+        } else if (hash < 16) {
+            if (hash < 14) {
+                if (hash < 13) {
+                    printf("this is branch 13\n");
+                } else if (hash < 14) {
+                    printf("this is branch 14\n");
+                }
+            } else if (hash < 16) {
+                if (hash < 15) {
+                    printf("this is branch 15\n");
+                } else if (hash < 16) {
+                    printf("this is branch 16\n");
+                }
+            }
+        }
+    }
+
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    uint8_t hash = 0;
+    for (size_t i = 0; i < size; ++i) {
+        hash += data[i];
+        printf("hash: %d\n", hash);
+    }
+    COMP_W2_D4_B1(hash % (int)pow(2, 4));
+    return 0;
+}
---
+++ COMP_W6_D2_B1/COMP_W6_D2_B1.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <math.h>
+
+void COMP_W6_D2_B1(unsigned hash)
+{
+    if (hash < 6) {
+        if (hash < 1) {
+            printf("this is branch 1\n");
+            int *ptr = NULL;
+            *ptr = 10;
+        } else if (hash < 2) {
+            printf("this is branch 2\n");
+        } else if (hash < 3) {
+            printf("this is branch 3\n");
+        } else if (hash < 4) {
+            printf("this is branch 4\n");
+        } else if (hash < 5) {
+            printf("this is branch 5\n");
+        } else if (hash < 6) {
+            printf("this is branch 6\n");
+        }
+    } else if (hash < 12) {
+        if (hash < 7) {
+            printf("this is branch 7\n");
+        } else if (hash < 8) {
+            printf("this is branch 8\n");
+        } else if (hash < 9) {
+            printf("this is branch 9\n");
+        } else if (hash < 10) {
+            printf("this is branch 10\n");
+        } else if (hash < 11) {
+            printf("this is branch 11\n");
+        } else if (hash < 12) {
+            printf("this is branch 12\n");
+        }
+    } else if (hash < 18) {
+        if (hash < 13) {
+            printf("this is branch 13\n");
+        } else if (hash < 14) {
+            printf("this is branch 14\n");
+        } else if (hash < 15) {
+            printf("this is branch 15\n");
+        } else if (hash < 16) {
+            printf("this is branch 16\n");
+        } else if (hash < 17) {
+            printf("this is branch 17\n");
+        } else if (hash < 18) {
+            printf("this is branch 18\n");
+        }
+    } else if (hash < 24) {
+        if (hash < 19) {
+            printf("this is branch 19\n");
+        } else if (hash < 20) {
+            printf("this is branch 20\n");
+        } else if (hash < 21) {
+            printf("this is branch 21\n");
+        } else if (hash < 22) {
+            printf("this is branch 22\n");
+        } else if (hash < 23) {
+            printf("this is branch 23\n");
+        } else if (hash < 24) {
+            printf("this is branch 24\n");
+        }
+    } else if (hash < 30) {
+        if (hash < 25) {
+            printf("this is branch 25\n");
+        } else if (hash < 26) {
+            printf("this is branch 26\n");
+        } else if (hash < 27) {
+            printf("this is branch 27\n");
+        } else if (hash < 28) {
+            printf("this is branch 28\n");
+        } else if (hash < 29) {
+            printf("this is branch 29\n");
+        } else if (hash < 30) {
+            printf("this is branch 30\n");
+        }
+    } else if (hash < 36) {
+        if (hash < 31) {
+            printf("this is branch 31\n");
+        } else if (hash < 32) {
+            printf("this is branch 32\n");
+        } else if (hash < 33) {
+            printf("this is branch 33\n");
+        } else if (hash < 34) {
+            printf("this is branch 34\n");
+        } else if (hash < 35) {
+            printf("this is branch 35\n");
+        } else if (hash < 36) {
+            printf("this is branch 36\n");
+        }
+    }
+
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    uint8_t hash = 0;
+    for (size_t i = 0; i < size; ++i) {
+        hash += data[i];
+        printf("hash: %d\n", hash);
+    }
+    COMP_W6_D2_B1(hash % (int)pow(6, 2));
+    return 0;
+}
